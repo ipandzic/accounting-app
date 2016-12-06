03b436a848d8c8ad1dc04499153c09ae1654ff7e
Pandza11 <ivan.pandzic@gmail.com>
2016-12-06 20:38:17 +0100

    Solved DateField issues

--- core/models.py
+++ core/models.py
@@ -1,51 +1,58 @@
 from __future__ import unicode_literals
-from decimal import Decimal
-import datetime
-from datetime import date, timedelta
 from django.db import models
 
+import datetime
+from datetime import date, timedelta
+
+from django.utils.translation import gettext as _
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+from decimal import Decimal
+
+from django.utils.timezone import datetime
+import calendar
 
 # Create your models here.
-
-def get_first_day(dt, d_years=0, d_months=0):
-	# d_years, d_months are "deltas" to apply to dt
-	y, m = dt.year + d_years, dt.month + d_months
-	a, m = divmod(m-1, 12)
-	return date(y+a, m+1, 1)
-
-def get_last_day(dt):
-	return get_first_day(dt, 0, 1) + timedelta(-1)
-
-
-
 
 class Project(models.Model):
 	'''Describes a project that transactions, 
 	clients, or people relate to.'''
+	def __str__(self):
+		return self.name
+
 	name = models.CharField(max_length=1024)
 	is_internal = models.BooleanField(default=False)
 	is_active = models.BooleanField(default=False)
 
+
 class Party(models.Model):
 	'''represents an entity which paid the company, 
-	or which was paid by the company. 
-	'''
+	or which was paid by the company.'''
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name_plural = "parties"
 
 	name = models.CharField(max_length=1024)
 	is_domestic = models.BooleanField(default=True)
 	vat_ptc = models.IntegerField(
 		default=25, validators=[MaxValueValidator(50), MinValueValidator(0)]
 		)
-	#projects = models.ManyToManyField()
+	projects = models.ManyToManyField(Project)
 	is_active = models.BooleanField(default=False)
+
 
 class Transaction(models.Model):
 	'''Represents any financial transaction 
-	between a company and some party.
-	'''
-	party = models.ForeignKey(Party)
+	between a company and some party.'''
+	#def __str__(self):
+	#	return self.name
+
+	def first_day_of_month(d):
+		return date(d.year, d.month, 1)
+
+	party = models.ForeignKey(Party, on_delete=models.CASCADE)
 
 	DIRECTION_CHOICES = (
 	('1','incoming'),
@@ -68,33 +75,30 @@ class Transaction(models.Model):
 		choices=TYPE_CHOICES
 		)
 
-	#projects = models.ManyToManyField()
+	projects = models.ManyToManyField(Project, blank=True)
 
-	#start_date = models.DateField(_("Date"), default=get_first_day(date))
-	
-	#end_date = models.DateField(_("Date"), default=get_last_day(date))
-	
-	#invoice_date = models.DateField(_("Date"), default=date.today())
-	
-	invoice_amount = models.DecimalField(max_digits=8, decimal_places=2)
-	
+	today = datetime.today()
+	year, month = today.year, today.month
+	monthrange = calendar.monthrange(year, month)
+	first_day_of_month = str(year) + '-' + str(month) + '-01'
+	last_day_of_month = str(year) + '-' + str(month) + '-' + str(monthrange[1])
 
-	'''
-	def vat_ammount(self, invoice_amount, )
-
-	vat_ammount = models.DecimalField(
-		max_digits=8, invoice_amount * party.vat_pct
-		) 
-	'''
+	start_date = models.DateField(_("Start date"), default=first_day_of_month)
 	
-
-	#party.invoice_pct => party.vat_pct
+	end_date = models.DateField(_("End date"), default=last_day_of_month)
 	
+	invoice_date = models.DateField(_("Invoice date"), default=today)
+	
+	invoice_amount = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
+	#total = invoice_amount * Party.vat_ptc
+	vat_ammount = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
+
 	settlement_date = models.DateField(null=True)
 	
-	settlement_ammount = models.DateField(default=0.00)
+	settlement_ammount = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
 	
-	adjustment = models.DateField(default=0.00)
+	adjustment = models.DecimalField(max_digits=8, decimal_places=2, default=0.00)
 	
 	description = models.CharField(max_length=1024, default='')
 	
