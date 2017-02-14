7df515003ae7cfda14b15914bc759babe64d8754
Pandza11 <ivan.pandzic@gmail.com>
2017-02-14 17:46:45 +0100

    Used generic views with forms.py

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.views import generic
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.core.urlresolvers import reverse_lazy
 from .models import Party, Project, Transaction
+from .forms import PartyForm, ProjectForm, TransactionForm
 
 
 class Home(generic.ListView):
@@ -31,112 +31,61 @@ class ProjectList(generic.ListView):
 class PartyDetailView(generic.DetailView):
     model = Party
     template_name = 'core/party_detail.html'
+    form_class = PartyForm
 
 
 class ProjectDetailView(generic.DetailView):
     model = Project
     template_name = 'core/project_detail.html'
+    form_class = ProjectForm
 
 
 class TransactionDetailView(generic.DetailView):
     model = Transaction
     template_name = 'core/transaction_detail.html'
+    form_class = TransactionForm
 
 
-class PartyCreate(CreateView):
+class PartyCreate(generic.CreateView):
     model = Party
-    fields = [
-        "name",
-        "is_domestic",
-        "vat_ptc",
-        "is_active",
-        "projects"
-    ]
+    form_class = PartyForm
 
 
-class PartyUpdate(UpdateView):
-    model = Party
-    fields = [
-        "name",
-        "is_domestic",
-        "vat_ptc",
-        "is_active",
-        "projects"
-    ]
-
-    def get_initial(self):
-        initial = super(PartyUpdate, self).get_initial()
-        return initial
-
-
-class PartyDelete(DeleteView):
-    model = Party
-    success_url = reverse_lazy("core:home")
-
-
-class ProjectCreate(CreateView):
+class ProjectCreate(generic.CreateView):
     model = Project
-    fields = [
-        "name",
-        "is_internal",
-        "is_active"
-    ]
+    form_class = ProjectForm
 
 
-class ProjectUpdate(UpdateView):
-    model = Project
-    fields = [
-        "name",
-        "is_internal",
-        "is_active"
-    ]
-
-
-class ProjectDelete(DeleteView):
-    model = Project
-    success_url = reverse_lazy("core:home")
-
-
-class TransactionCreate(CreateView):
+class TransactionCreate(generic.CreateView):
     model = Transaction
-    fields = [
-        "party",
-        "direction",
-        "transaction_type",
-        "projects",
-        "start_date",
-        "end_date",
-        "invoice_date",
-        "invoice_amount",
-        "vat_ammount",
-        "settlement_date",
-        "settlement_ammount",
-        "adjustment",
-        "description",
-        "comments"
-        ]
+    form_class = TransactionForm
 
 
-class TransactionUpdate(UpdateView):
+class PartyUpdate(generic.UpdateView):
+    model = Party
+    form_class = PartyForm
+
+
+class ProjectUpdate(generic.UpdateView):
+    model = Project
+    form_class = ProjectForm
+
+
+class TransactionUpdate(generic.UpdateView):
     model = Transaction
-    fields = [
-        "party",
-        "direction",
-        "transaction_type",
-        "projects",
-        "start_date",
-        "end_date",
-        "invoice_date",
-        "invoice_amount",
-        "vat_ammount",
-        "settlement_date",
-        "settlement_ammount",
-        "adjustment",
-        "description",
-        "comments"
-        ]
+    form_class = TransactionForm
 
 
-class TransactionDelete(DeleteView):
+class PartyDelete(generic.DeleteView):
+    model = Party
+    success_url = reverse_lazy("core:party_list")
+
+
+class ProjectDelete(generic.DeleteView):
+    model = Project
+    success_url = reverse_lazy("core:project_list")
+
+
+class TransactionDelete(generic.DeleteView):
     model = Transaction
     success_url = reverse_lazy("core:home")
