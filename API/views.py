e2ac6eebe716abdd677f3af9052acfbd666f84a3
Pandza11 <ivan.pandzic@gmail.com>
2017-03-01 10:40:32 +0100

    Added post functionality to APIs

--- API/views.py
+++ API/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from core.models import Party, Project, Transaction
 from .serializers import PartySerializer, ProjectSerializer, TransactionSerializer
 
@@ -10,6 +11,13 @@ class PartyListAPI(APIView):
         serializer = PartySerializer(parties, many=True)
         return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = PartySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class ProjectListAPI(APIView):
     def get(self, request):
@@ -17,9 +25,23 @@ class ProjectListAPI(APIView):
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class TransactionListAPI(APIView):
     def get(self, request):
         transactions = Transaction.objects.all()
         serializer = TransactionSerializer(transactions, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = TransactionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
