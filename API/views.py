b0e12f02e45f33fe06ea7af853656e90eb1f3e80
Pandza11 <ivan.pandzic@gmail.com>
2017-03-01 11:24:55 +0100

    Rewritten API's views.up using generic class based views

--- API/views.py
+++ API/views.py
@@ -1,47 +1,33 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 from core.models import Party, Project, Transaction
 from .serializers import PartySerializer, ProjectSerializer, TransactionSerializer
 
 
-class PartyListAPI(APIView):
-    def get(self, request):
-        parties = Party.objects.all()
-        serializer = PartySerializer(parties, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = PartySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PartyListAPI(generics.ListCreateAPIView):
+    queryset = Party.objects.all()
+    serializer_class = PartySerializer
 
 
-class ProjectListAPI(APIView):
-    def get(self, request):
-        projects = Project.objects.all()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = ProjectSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class PartyDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Party.objects.all()
+    serializer_class = PartySerializer
 
 
-class TransactionListAPI(APIView):
-    def get(self, request):
-        transactions = Transaction.objects.all()
-        serializer = TransactionSerializer(transactions, many=True)
-        return Response(serializer.data)
+class ProjectListAPI(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
 
-    def post(self, request, format=None):
-        serializer = TransactionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProjectDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TransactionListAPI(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class TransactionDetailAPI(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
