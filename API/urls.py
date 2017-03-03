39d73831983f724bdd7855264d1795de4f279314
Pandza11 <ivan.pandzic@gmail.com>
2017-03-03 15:00:20 +0100

    Added detail views to API/urls.py

--- API/urls.py
+++ API/urls.py
@@ -4,7 +4,10 @@ from . import views
 app_name = 'API'
 
 urlpatterns = [
-    url(r'^party_list/', views.PartyListAPI.as_view()),
-    url(r'^project_list/', views.ProjectListAPI.as_view()),
-    url(r'^transaction_list/', views.TransactionListAPI.as_view()),
+    url(r'^party/$', views.PartyListAPI.as_view()),
+    url(r'^party/(?P<pk>[0-9]+)$', views.PartyDetailAPI.as_view()),
+    url(r'^project/$', views.ProjectListAPI.as_view()),
+    url(r'^project/(?P<pk>[0-9]+)$', views.ProjectDetailAPI.as_view()),
+    url(r'^transaction/$', views.TransactionListAPI.as_view()),
+    url(r'^transaction/(?P<pk>[0-9]+)$', views.TransactionDetailAPI.as_view()),
 ]
